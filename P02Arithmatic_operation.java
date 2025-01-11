6276fcbbbc303ee081a8f1abd6802435e281fc38
Saif Shaikh <shaikhsaif9500@gmail.com>
2025-01-11 23:04:14 +0530

    Arithmetic Operation

--- P02Arithmatic_operation.java
+++ P02Arithmatic_operation.java
@@ -1,8 +1,8 @@
 public class P02Arithmatic_operation {
     public static void main(String[]arrgs)
 {
-    int a=100;// declaring part
-    int b=20;
+    int a=100;
+        int b=20;
     System.out.println("The Addition is "+(a+b));
 
     System.out.println("The Subtraction is "+(a-b));
