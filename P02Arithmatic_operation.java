000a7ca21acd60f576f7c6b1d10606be5cf9eb6c
Saif Shaikh <shaikhsaif9500@gmail.com>
2025-01-11 22:58:40 +0530

    Create P02Arithmatic_operation.java
    Arithmetic operation

---
+++ P02Arithmatic_operation.java
@@ -0,0 +1,14 @@
+public class P02Arithmatic_operation {
+    public static void main(String[]arrgs)
+{
+    int a=100;
+    int b=20;
+    System.out.println("The Addition is "+(a+b));
+
+    System.out.println("The Subtraction is "+(a-b));
+    System.out.println("The Multiplication  is "+(a*b));
+    System.out.println("The Division is "+(a/b));
+    System.out.println("The Modulus is "+(a%b));
+
+}    
+}
