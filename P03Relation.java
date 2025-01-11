5d660e6c6338340f8e332671676ae43093471f86
Saif Shaikh <shaikhsaif9500@gmail.com>
2025-01-11 22:58:56 +0530

    Relational operator

---
+++ P03Relation.java
@@ -0,0 +1,14 @@
+public class P03Relation //class name same as file name is compalsary
+{                     // my file seqnce of other name
+    public static void main(String[]arrgs)
+    {
+        int A=7,B=6;
+            System.out.println("A>b   "+(A>B)); //true
+            System.out.println("A<b   "+(A<B)); //false
+            System.out.println("A==b  "+(A==B)); //false
+            System.out.println("A!=b  "+(A>B)); //true
+            System.out.println("A<=b  "+(A<=B)); //false
+            System.out.println("A>=b  "+(A>B)); //true
+
+    }
+}
